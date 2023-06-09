745dfa0db1b586425f4147cbcdc4b4c01a7135a4
Lizbeth Castro <lizbethcastro010@gmail.com>
2023-06-09 14:11:16 -0600

    Edited schema page

--- schema.sql
+++ schema.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE employees_db;
 
+USE employees_db;
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
